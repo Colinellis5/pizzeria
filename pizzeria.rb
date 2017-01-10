875d20f3e89a18e0a5bf8511cabb87d30f902d28
Colinellis5 <colin@colinellis.nl>
2017-01-10 09:22:16 +0100

    Ask user for their name

---
+++ pizzeria.rb
@@ -0,0 +1,11 @@
+def initialize_pizzas
+  @pizza_salami = 4.0
+  @pizza_quattro_stagioni = 8.5
+  @pizza_ham_cheese = 6.25
+  @pizza_shoarma = 7.3
+end
+
+def customer_name
+  print "What's your name? "
+  return gets.chomp
+end
